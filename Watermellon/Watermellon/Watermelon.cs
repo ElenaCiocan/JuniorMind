cd15a7cb3abfac2a7bfc48017050235fd7d323b2
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-14 22:07:18 +0200

    Test for negative answer
    Check if the number is odd

--- Watermellon/Watermellon/Watermelon.cs
+++ Watermellon/Watermellon/Watermelon.cs
@@ -11,12 +11,18 @@ namespace Watermelon
         {
             Assert.AreEqual("DA", VerifyParity(4));
         }
+        [TestMethod]
+        public void OddNumberOfKilograms()
+        {
+            Assert.AreEqual("Nu", VerifyParity(5));
+        }
 
         string VerifyParity(int numberOfKilograms)
         {
             if (numberOfKilograms % 2 == 0)
                 return "DA";
-          return Convert.ToString(numberOfKilograms);
+            else
+                return "Nu";
        
         }
     }
