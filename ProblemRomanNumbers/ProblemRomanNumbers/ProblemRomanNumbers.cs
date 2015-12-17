11748a2270245bcc12699c003d3a2f699ecfa6a5
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-17 23:42:43 +0200

    Test for conversion of one

--- ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
+++ ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
@@ -4,11 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ProblemRomanNumbers
 {
     [TestClass]
-    public class UnitTest1
+    public class ProblemRomanNumbers
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ConversionOfOne()
         {
+            Assert.AreEqual("I", ConvertToRomanNumbers(1));
+        }
+
+        string ConvertToRomanNumbers(int number)
+        {
+                return "I";
         }
     }
 }
