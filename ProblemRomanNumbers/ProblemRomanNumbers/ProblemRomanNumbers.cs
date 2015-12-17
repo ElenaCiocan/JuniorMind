e1e84f7b1a609b07fce081471bd81938d052b19b
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-18 00:30:38 +0200

    Test for conversion of twenty five

--- ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
+++ ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
@@ -18,10 +18,19 @@ namespace ProblemRomanNumbers
             Assert.AreEqual("IX", ConvertToRomanNumbers(9));
         }
 
+        [TestMethod]
+        public void ConversionOfTwentyFive()
+        {
+            Assert.AreEqual("XXV", ConvertToRomanNumbers(25));
+        }
+
         string ConvertToRomanNumbers(int number)
         {
-            string[] romanunits = new string[] { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-            return romanunits[number-1];
+            string result = string.Empty;
+            string[] romanunits = new string[] { "","I","II","III","IV","V","VI","VII","VIII","IX"};
+            string[] romantens = new string[] { "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C"};
+            result =romantens[number / 10]+romanunits[number % 10 ];
+                return result;
         }
     }
 }
