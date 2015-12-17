488bb5bc716fadb86fb50e0491f33fb9d153422a
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-17 23:57:42 +0200

    Test for conversion of nine

--- ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
+++ ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
@@ -12,9 +12,16 @@ namespace ProblemRomanNumbers
             Assert.AreEqual("I", ConvertToRomanNumbers(1));
         }
 
+        [TestMethod]
+        public void ConversionOfNine()
+        {
+            Assert.AreEqual("IX", ConvertToRomanNumbers(9));
+        }
+
         string ConvertToRomanNumbers(int number)
         {
-                return "I";
+            string[] romanunits = new string[] { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            return romanunits[number-1];
         }
     }
 }
