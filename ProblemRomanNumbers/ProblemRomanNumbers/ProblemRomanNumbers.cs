8e0201337abab5705a0eed98cb667565b955dee1
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-18 00:46:47 +0200

    Test for conversion of nine hundred ninety four

--- ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
+++ ProblemRomanNumbers/ProblemRomanNumbers/ProblemRomanNumbers.cs
@@ -24,13 +24,18 @@ namespace ProblemRomanNumbers
             Assert.AreEqual("XXV", ConvertToRomanNumbers(25));
         }
 
+        [TestMethod]
+        public void ConversionOfNineHundredNinetyFour()
+        {
+            Assert.AreEqual("CMXCIV", ConvertToRomanNumbers(994));
+        }
+
         string ConvertToRomanNumbers(int number)
         {
-            string result = string.Empty;
-            string[] romanunits = new string[] { "","I","II","III","IV","V","VI","VII","VIII","IX"};
-            string[] romantens = new string[] { "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C"};
-            result =romantens[number / 10]+romanunits[number % 10 ];
-                return result;
+            string[] romanunits = new string[] {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+            string[] romantens = new string[] { "","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",};
+            string[] romanhundreds = new string[] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "M" };
+            return romanhundreds[number/100] + romantens[number % 100 / 10] + romanunits[number % 10];
         }
     }
 }
