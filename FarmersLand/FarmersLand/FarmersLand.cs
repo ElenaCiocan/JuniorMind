5d6779f629307b7f8440631377f45cb8af6820cd
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-18 10:17:49 +0200

    Test for negative values

--- FarmersLand/FarmersLand/FarmersLand.cs
+++ FarmersLand/FarmersLand/FarmersLand.cs
@@ -18,12 +18,23 @@ namespace FarmersLand
             Assert.AreEqual(770, CalculateTheInitialDimensionOfTheLand(230, 770000));
         }
 
+        [TestMethod]
+        public void TestForNegativeValues()
+        {
+            Assert.AreEqual(0, CalculateTheInitialDimensionOfTheLand(230, -770000));
+        }
+
         double CalculateTheInitialDimensionOfTheLand(int width, int finalArea)
         {
             double delta = width * width - (4 * 1 * (-1) * finalArea);
-            double initialLength = (-width + Math.Sqrt(delta)) / 2*1;
-            // we use only minus because a length can not be negative
-            return initialLength;
+            if (delta > 0)
+            {
+                double initialLength = (-width + Math.Sqrt(delta)) / 2 * 1;
+                // we use only minus because a length can not be negative
+                return initialLength;
+            }
+            else
+                return 0;
         }
     }
 }
