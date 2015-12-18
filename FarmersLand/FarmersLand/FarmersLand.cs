45b8515f8ca806156d98a6b6b004dfa9b49e6bca
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-18 10:12:57 +0200

    Test for small dimensions

--- FarmersLand/FarmersLand/FarmersLand.cs
+++ FarmersLand/FarmersLand/FarmersLand.cs
@@ -4,11 +4,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FarmersLand
 {
     [TestClass]
-    public class UnitTest1
+    public class FarmersLand
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForSmallDimensions()
         {
+            Assert.AreEqual(2, CalculateTheInitialDimensionOfTheLand(4, 12));
+        }
+
+        double CalculateTheInitialDimensionOfTheLand(int width, int finalArea)
+        {
+            double delta = width * width - (4 * 1 * (-1) * finalArea);
+            double initialLength = (-width + Math.Sqrt(delta)) / 2*1;
+            // we use only minus because a length can not be negative
+            return initialLength;
         }
     }
 }
