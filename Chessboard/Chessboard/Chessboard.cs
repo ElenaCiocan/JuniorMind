d9942b72005a70f5793b32f1a2c0e4b20dbf520c
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-28 19:37:34 +0200

    Test for a Chessboard 8X8

--- Chessboard/Chessboard/Chessboard.cs
+++ Chessboard/Chessboard/Chessboard.cs
@@ -12,9 +12,19 @@ namespace Chessboard
             Assert.AreEqual(5, CalculateTheSquaresOnAChessboard(2));
         }
 
+        [TestMethod]
+        public void TestForAEightByEightChessboard()
+        {
+            Assert.AreEqual(204, CalculateTheSquaresOnAChessboard(8));
+        }
+
         int CalculateTheSquaresOnAChessboard(int lenght)
         {
-            return lenght * lenght + 1;
+            int noOfSquares = 0;
+            for (int i = 1; i <= lenght; i++)
+                noOfSquares += (i * i);
+            return noOfSquares;
         }
+
     }
 }
