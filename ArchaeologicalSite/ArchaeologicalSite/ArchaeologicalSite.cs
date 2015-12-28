2c5d390c3a674cc84a25ab4a18b83545db7b6ded
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-28 14:31:32 +0200

    Test for small values without decimals

--- ArchaeologicalSite/ArchaeologicalSite/ArchaeologicalSite.cs
+++ ArchaeologicalSite/ArchaeologicalSite/ArchaeologicalSite.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ArchaeologicalSite
 {
     [TestClass]
-    public class UnitTest1
+    public class ArchaeologicalSite
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForSmallValues()
         {
+            Assert.AreEqual(6, CalculateTheAreaOfTheSit(0, 0, 3, 0, 0, 4)); 
         }
-    }
+
+        double CalculateTheAreaOfTheSit(double xA, double yA, double xB, double yB, double xC, double yC)
+        {
+            double determinant = (xA * yB * 1) + (xB * yC * 1) + (yA * xC * 1) - (yB * xC * 1) - (yC * xA * 1) - (xB * yA * 1);
+            return Math.Abs(determinant) / 2;
+        }
+
+    } 
 }
