1a0a5b4bd708e43ee67e747e604e1f64589d7ee1
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-28 14:49:42 +0200

    Test for decimal numbers

--- ArchaeologicalSite/ArchaeologicalSite/ArchaeologicalSite.cs
+++ ArchaeologicalSite/ArchaeologicalSite/ArchaeologicalSite.cs
@@ -12,6 +12,11 @@ namespace ArchaeologicalSite
             Assert.AreEqual(6, CalculateTheAreaOfTheSit(0, 0, 3, 0, 0, 4)); 
         }
 
+        [TestMethod]
+        public void TestForDecimalValues()
+        {
+            Assert.AreEqual(27.2949242226, CalculateTheAreaOfTheSit(0,0,6.721800,0,0,8.121314));
+        }
         double CalculateTheAreaOfTheSit(double xA, double yA, double xB, double yB, double xC, double yC)
         {
             double determinant = (xA * yB * 1) + (xB * yC * 1) + (yA * xC * 1) - (yB * xC * 1) - (yC * xA * 1) - (xB * yA * 1);
