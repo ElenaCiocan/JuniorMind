435892bf379ec36b8dc977f28fa672dcaeff3d15
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-28 20:18:11 +0200

    Test for finding the common prefix of two strings

--- Prefix/Prefix/Prefix.cs
+++ Prefix/Prefix/Prefix.cs
@@ -4,11 +4,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Prefix
 {
     [TestClass]
-    public class UnitTest1
+    public class Prefix
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FindThePrefixOfTwoStrings()
         {
+            Assert.AreEqual("aaa", FindThePrefix("aaab", "aaaabbaa"));
+        }
+
+        string FindThePrefix(string firstString, string secondsString)
+        {
+            int noOfEqualLetters=0,i;
+            string prefix=null;
+            for (i = 0; i < Math.Min(firstString.Length, secondsString.Length); i++)
+                if (firstString[i] == secondsString[i])
+                    noOfEqualLetters++;
+                else
+                    break;
+            for (i = 0; i < noOfEqualLetters; i++)
+                prefix += firstString[i];
+            return prefix;
         }
     }
 }
