f040a240438958f722db58d4e9540d92cdea8ba0
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-28 15:38:36 +0200

    Test for medium periods of time

--- Debt/Debt/Debt.cs
+++ Debt/Debt/Debt.cs
@@ -7,15 +7,26 @@ namespace Debt
     public class Debt
     {
         [TestMethod]
-        public void TestForLessThanTenDays()
+        public void TestForShortPeriod()
         {
             Assert.AreEqual(104, CalculateTheDebt(2, 100));
         }
 
+        [TestMethod]
+        public void TestForMediumPeriod()
+        {
+            Assert.AreEqual(160, CalculateTheDebt(12, 100));
+        }
+
         float CalculateTheDebt(int days, float rent)
         {
-            float percentage =(float) 2 / 100;
-            return rent + ( percentage * rent * days);
+            float shortPeriodPercentage =(float) 2 / 100;
+            float mediumPeriodPercentage = (float)5 / 100;
+            if (days>11)
+            return rent + ( mediumPeriodPercentage * rent * days);
+            else
+                return rent + (shortPeriodPercentage * rent * days);
+
         }
     }
 }
