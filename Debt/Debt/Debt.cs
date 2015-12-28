405a912c3c178ae253bba1757758b9d2e2bcdc9a
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-28 16:07:42 +0200

    Test for periods longer than the given ones

--- Debt/Debt/Debt.cs
+++ Debt/Debt/Debt.cs
@@ -24,20 +24,24 @@ namespace Debt
             Assert.AreEqual(450, CalculateTheDebt(35, 100));
         }
 
+        [TestMethod]
+        public void TestForMoreThanFortyDays()
+        {
+            Assert.AreEqual(0, CalculateTheDebt(41, 100));
+        }
+
         float CalculateTheDebt(int days, float rent)
         {
-            float shortPeriodPercentage =(float) 2 / 100;
+            float shortPeriodPercentage = (float) 2 / 100;
             float mediumPeriodPercentage = (float) 5 / 100;
             float longPeriodPercentage = (float) 10 / 100;
-            if (days>31)
-                return rent + ( longPeriodPercentage * rent * days);
-            else
-                if(days>11)
+            if (days >= 31 && days <=40)
+                return rent + (longPeriodPercentage * rent * days);
+            if (days >= 11 && days <=30)
                 return rent + (mediumPeriodPercentage * rent * days);
-            else
+            if (days >=1 && days <=10 ) 
                 return rent + (shortPeriodPercentage * rent * days);
-
-
+            return 0;
         }
     }
 }
