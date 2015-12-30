8d535532e63217c30b2cc5c2a508b4ceb62055a1
ElenaCiocan <elenaciocan94@gmail.com>
2015-12-30 14:31:54 +0200

    Conversion of all the letters in lower case letters
    It is the first step in solving Pangram problem

--- Pangram/Pangram/Pangram.cs
+++ Pangram/Pangram/Pangram.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Pangram
 {
     [TestClass]
-    public class UnitTest1
+    public class Pangram
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ModifyAllTheLettersInLowerCaseLetters()
         {
+            Assert.AreEqual("abcdefghijklmnopqrstuvwxyz", ConvertUpperCaseLettersToLowerCase("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        }
+        
+        String ConvertUpperCaseLettersToLowerCase(string sentence)
+        {
+            System.Text.StringBuilder givenSentence = new System.Text.StringBuilder(sentence);
+            for (int i = 0; i < givenSentence.Length; i++)
+                if (System.Char.IsUpper(givenSentence[i]) == true)
+                    givenSentence[i] = System.Char.ToLower(givenSentence[i]);
+            return givenSentence.ToString();            
         }
     }
 }
