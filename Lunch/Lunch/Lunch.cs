039950694c8ba11f7fedf90d6f72de136ba924d5
ElenaCiocan <elenaciocan94@gmail.com>
2016-01-06 23:36:29 +0200

    Test to find the first meeting

--- Lunch/Lunch/Lunch.cs
+++ Lunch/Lunch/Lunch.cs
@@ -4,11 +4,27 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Lunch
 {
     [TestClass]
-    public class UnitTest1
+    public class Lunch
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForTheFirstMeeting()
         {
+            Assert.AreEqual(12, CalculateDayOfMeeting(4,6));
+        }
+
+        public int CalculateDayOfMeeting(int firstPersonDay, int secondPersonDay)
+        {
+            int meetingDay=0,i;
+            int product = firstPersonDay * secondPersonDay;
+            for (i = 1; i <= product; i++)
+            {
+                if ((i % firstPersonDay == 0) && (i % secondPersonDay == 0))
+                {
+                    meetingDay = i;
+                    break;
+                }
+            }
+            return meetingDay;
         }
     }
 }
