444f581a0aca768d6428c6486a9687edbacbb098
ElenaCiocan <elenaciocan94@gmail.com>
2016-01-09 00:58:14 +0200

    Second step: find the unique characters

--- Anagrams/Anagrams/Anagrams.cs
+++ Anagrams/Anagrams/Anagrams.cs
@@ -12,12 +12,31 @@ namespace Anagrams
             Assert.AreEqual(6, CalculateFactorial(3));
         }
 
+        [TestMethod]
+        public void ShouldFindOnlyUniqueCharacters()
+        {
+            Assert.AreEqual("abcdefzg", FindEachUniqueChar("aaabbbcccddddefzggg"));
+        }
+
         public int CalculateFactorial(int number)
         {
             int factorial=1;
             for (int i = 1; i <= number; i++)
                 factorial *= i;
             return factorial;
+        }
+
+        public String FindEachUniqueChar(string word)
+        {
+            string uniqueChar = String.Empty;
+            int[] check = new int[27];
+            for (int i = 0; i < word.Length; i++)
+                if (check[(word[i] - 96)] == 0)
+                {
+                    uniqueChar += word[i];
+                    check[word[i] - 96] = 1;
+                }
+            return uniqueChar;
         }
     }
 }
