1da7dcd310cc9441e4cba778a4ab69ca36084ed4
ElenaCiocan <elenaciocan94@gmail.com>
2016-01-14 11:12:10 +0200

    Modified version of Loto

--- Loto/Loto/Loto.cs
+++ Loto/Loto/Loto.cs
@@ -9,37 +9,44 @@ namespace Loto
         [TestMethod]
         public void TestForTheFirstCategory()
         {
-            Assert.AreEqual(0.0000000715, CalculateChances(6,49));
+            Assert.AreEqual(0.0000000715, CalculateChances(6,6,49));
         }
 
         [TestMethod]
         public void TestForTheSecondCategory()
         {
-            Assert.AreEqual(0.0000005244, CalculateChances(5,49));
+            Assert.AreEqual(0.0000184499, CalculateChances(5,6,49));
         }
 
         [TestMethod]
         public void TestForTheThirdCategory()
         {
-            Assert.AreEqual(0.0000047197, CalculateChances(4,49));
+            Assert.AreEqual(0.0009686197, CalculateChances(4,6,49));
         }
 
         [TestMethod]
         public void TestForTheFirstCategoryAtFiveFromForty()
         {
-            Assert.AreEqual(0.0000015197, CalculateChances(5, 40));
+            Assert.AreEqual(0.0000015197, CalculateChances(5,5, 40));
         }
 
-        public double CalculateChances(int myNumbers, double total)
+        public double CalculateChances(double matchingNumbers, double ticketNumbers, double totalNumbers)
         {
-            double chances=1;
-            while(myNumbers>0)
+            double result = 1;
+            result = CalculateFraction(ticketNumbers, matchingNumbers) * CalculateFraction((totalNumbers - ticketNumbers), (ticketNumbers - matchingNumbers));
+            result /= CalculateFraction(totalNumbers, ticketNumbers);
+            return Math.Round(result, 10);
+        }
+        public double CalculateFraction(double numerator, double denominator)
+        {
+            double result=1;
+            while(Math.Min(numerator,denominator)>0)
             {
-                chances *= (myNumbers / total);
-                myNumbers--;
-                total--;
+                result *= (numerator/ denominator);
+                numerator--;
+                denominator--;
             }
-            return Math.Round(chances, 10);
+            return result;
         }
     }
 }
