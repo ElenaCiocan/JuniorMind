811eeccf2032c8ae36c13e09d5204292808dfa3e
ElenaCiocan <elenaciocan94@gmail.com>
2016-01-14 10:19:35 +0200

    Test for 5 from 40

--- Loto/Loto/Loto.cs
+++ Loto/Loto/Loto.cs
@@ -9,24 +9,30 @@ namespace Loto
         [TestMethod]
         public void TestForTheFirstCategory()
         {
-            Assert.AreEqual(0.0000000715, CalculateChances(6));
+            Assert.AreEqual(0.0000000715, CalculateChances(6,49));
         }
 
         [TestMethod]
         public void TestForTheSecondCategory()
         {
-            Assert.AreEqual(0.0000005244, CalculateChances(5));
+            Assert.AreEqual(0.0000005244, CalculateChances(5,49));
         }
 
         [TestMethod]
         public void TestForTheThirdCategory()
         {
-            Assert.AreEqual(0.0000047197, CalculateChances(4));
+            Assert.AreEqual(0.0000047197, CalculateChances(4,49));
         }
 
-        public double CalculateChances(int myNumbers)
+        [TestMethod]
+        public void TestForTheFirstCategoryAtFiveFromForty()
         {
-            double chances=1, total = 49;
+            Assert.AreEqual(0.0000015197, CalculateChances(5, 40));
+        }
+
+        public double CalculateChances(int myNumbers, double total)
+        {
+            double chances=1;
             while(myNumbers>0)
             {
                 chances *= (myNumbers / total);
