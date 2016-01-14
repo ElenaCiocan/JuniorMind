53db7bff186294356ab34f4de33529f7da2b5689
ElenaCiocan <elenaciocan94@gmail.com>
2016-01-14 10:04:35 +0200

    Test for the first category

--- Loto/Loto/Loto.cs
+++ Loto/Loto/Loto.cs
@@ -4,11 +4,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Loto
 {
     [TestClass]
-    public class UnitTest1
+    public class Loto
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForTheFirstCategory()
         {
+            Assert.AreEqual(0.00000007, CalculateChances(6));
+        }
+
+        public double CalculateChances(int myNumbers)
+        {
+            double chances=1, total = 49;
+            while(myNumbers>0)
+            {
+                chances *= (myNumbers / total);
+                myNumbers--;
+                total--;
+            }
+            return Math.Round(chances, 8);
         }
     }
 }
