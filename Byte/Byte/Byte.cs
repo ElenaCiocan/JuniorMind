10e6cd9e2e85cc641a6b79c28f16ffb69af9ac4b
ElenaCiocan <elenaciocan94@gmail.com>
2016-02-27 12:59:51 +0200

    Conversion to binary

--- Byte/Byte/Byte.cs
+++ Byte/Byte/Byte.cs
@@ -4,11 +4,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Byte
 {
     [TestClass]
-    public class UnitTest1
+    public class Byte
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForConversionToBinary()
         {
+            CollectionAssert.AreEqual(new byte[]{1,0}, ConvertToBinary(2));
+            CollectionAssert.AreEqual(new byte[] { 1,1,0,0 }, ConvertToBinary(12));
+
+        }
+
+        byte[] ConvertToBinary ( int number )
+        {
+            byte[] binaryNumber = new byte[0];
+            int i = 0;
+            while(number>0)
+            {
+                Array.Resize(ref binaryNumber, i+1);
+                binaryNumber[i++] = (byte)(number % 2);
+                number /= 2;
+            }
+            Array.Reverse(binaryNumber);
+            return binaryNumber;
         }
     }
 }
