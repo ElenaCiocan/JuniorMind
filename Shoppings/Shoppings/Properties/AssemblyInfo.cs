ff162630f7136d3af74c33f78ac1db78184693d5
ElenaCiocan <elenaciocan94@gmail.com>
2016-03-10 14:39:22 +0200

    Test for the calculation of the total price

--- Shoppings/Shoppings/Properties/AssemblyInfo.cs
+++ Shoppings/Shoppings/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9887df75-d374-4d17-abad-555598ea2db0")]
+[assembly: Guid("908b92d7-53b7-4c74-8fd0-f07cb5cd21db")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Shoppings/Shoppings/Shoppings.cs
+++ Shoppings/Shoppings/Shoppings.cs
@@ -4,11 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Shoppings
 {
     [TestClass]
-    public class UnitTest1
+    public class Shoppings
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForFindingTheTotal()
         {
+            var products = new Product[] { new Product("Cake", 50), new Product("Coke", (decimal)10.50), new Product("Chips", 25), new Product("Chocolate", (decimal)15.25) };
+            Assert.AreEqual((decimal)100.75, CalculateTotal(products));
+
+        }
+
+        struct Product
+        {
+            public string name;
+            public decimal price;
+            public Product(string name, decimal price)
+            {
+                this.name = name;
+                this.price = price;
+            }
+        }
+
+        decimal CalculateTotal( Product[] products)
+        {
+            decimal total=0;
+            for (int i = 0; i < products.Length; i++)
+                total += products[i].price;
+            return total;
         }
     }
 }
