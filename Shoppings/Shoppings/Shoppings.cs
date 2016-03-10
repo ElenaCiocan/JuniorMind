18cf2bcd3ec5e963d601603fdd9c9dd56e9ede90
ElenaCiocan <elenaciocan94@gmail.com>
2016-03-10 14:51:19 +0200

    Test for discover the cheapest product

--- Shoppings/Shoppings/Shoppings.cs
+++ Shoppings/Shoppings/Shoppings.cs
@@ -11,8 +11,16 @@ namespace Shoppings
         {
             var products = new Product[] { new Product("Cake", 50), new Product("Coke", (decimal)10.50), new Product("Chips", 25), new Product("Chocolate", (decimal)15.25) };
             Assert.AreEqual((decimal)100.75, CalculateTotal(products));
+        }
+
+        [TestMethod]
+        public void TestForFindingTheCheapestProduct()
+        {
+            var products = new Product[] { new Product("Cake", 50), new Product("Coke", (decimal)15.50), new Product("Chips", 25), new Product("Chocolate", (decimal)8.25) };
+            Assert.AreEqual("Chocolate", FindOutTheCheapestProduct(products));
 
         }
+
 
         struct Product
         {
@@ -31,6 +39,19 @@ namespace Shoppings
             for (int i = 0; i < products.Length; i++)
                 total += products[i].price;
             return total;
+        }
+
+        string FindOutTheCheapestProduct( Product[] products)
+        {
+            decimal theCheapestProduct = products[0].price;
+            int noProduct = 0;
+            for (int i = 1; i < products.Length; i++)
+                if (products[i].price < theCheapestProduct)
+                {
+                    theCheapestProduct = products[i].price;
+                    noProduct = i;
+                }
+            return products[noProduct].name;
         }
     }
 }
