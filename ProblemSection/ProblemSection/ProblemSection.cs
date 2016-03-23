522a6c230c1987c02ea87f9ace07f7407637df4c
ElenaCiocan <elenaciocan94@gmail.com>
2016-03-23 17:07:23 +0200

    Test for finding the next point

--- ProblemSection/ProblemSection/ProblemSection.cs
+++ ProblemSection/ProblemSection/ProblemSection.cs
@@ -4,11 +4,47 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ProblemSection
 {
     [TestClass]
-    public class UnitTest1
+    public class ProblemSection
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForNextPoint()
         {
+            var point = new Point(2,6);
+            var initPoint = new Point(2, 3);
+            Assert.AreEqual(point, CalculateNextPoint(initPoint, Direction.Up, 3));
+        }
+
+        struct Point
+        {
+           public double x;
+           public double y;
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        enum Direction
+        {
+            Right=1,
+            Left,
+            Up,
+            Down
+        }
+
+        Point CalculateNextPoint( Point startingPoint, Direction direction, int distance)
+        {
+            if (direction.Equals(Direction.Right))
+                startingPoint.x += distance;
+            if (direction.Equals(Direction.Left))
+                startingPoint.x -= distance;
+            if (direction.Equals(Direction.Up))
+                startingPoint.y += distance;
+            if (direction.Equals(Direction.Right))
+                startingPoint.y -= distance;
+            return startingPoint;
         }
     }
 }
