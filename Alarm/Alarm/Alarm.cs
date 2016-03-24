59f50780083d00bf39307f12211c8ca21db4baa7
ElenaCiocan <elenaciocan94@gmail.com>
2016-03-24 02:07:18 +0200

    Test for finding out if the alarm is set

--- Alarm/Alarm/Alarm.cs
+++ Alarm/Alarm/Alarm.cs
@@ -4,11 +4,52 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Alarm
 {
     [TestClass]
-    public class UnitTest1
+    public class Alarm
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForTheAlarm()
         {
+            Alarms[] alarms = {
+                new Alarms { hour = 7, day = DaysOfTheWeek.Monday | DaysOfTheWeek.Tuesday | DaysOfTheWeek.Wednesday | DaysOfTheWeek.Thursday | DaysOfTheWeek.Friday },
+                new Alarms { hour = 8, day = DaysOfTheWeek.Saturday | DaysOfTheWeek.Sunday }
+            };
+            Assert.AreEqual(true, VerifyTheAlarm(8, DaysOfTheWeek.Saturday, alarms));
+            Assert.AreEqual(false, VerifyTheAlarm(7, DaysOfTheWeek.Sunday, alarms));
+
+        }
+
+        [Flags]
+        enum DaysOfTheWeek
+        {
+            Monday=1,
+            Tuesday=2,
+            Wednesday=4,
+            Thursday=8,
+            Friday=16,
+            Saturday=32,
+            Sunday=64
+        }
+
+        struct Alarms
+        {
+           public int hour;
+            public DaysOfTheWeek day;
+
+            public Alarms(int hour, DaysOfTheWeek day)
+            {
+                this.hour = hour;
+                this.day = day;
+            }
+        }
+
+        bool VerifyTheAlarm(int hour, DaysOfTheWeek day, Alarms[] allAlarms)
+        {
+            for (int i = 0; i < allAlarms.Length; i++)
+            {
+                if( ((day & allAlarms[i].day) !=0) && ((hour & allAlarms[i].hour) !=0))
+                    return true;
+            }
+            return false;
         }
     }
 }
