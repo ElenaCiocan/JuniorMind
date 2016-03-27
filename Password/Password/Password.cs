fff328db6b18ce36ea4187cf6f5bdc01865ba26a
ElenaCiocan <elenaciocan94@gmail.com>
2016-03-27 15:45:38 +0300

    Test for generate small letters

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -4,11 +4,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Password
 {
     [TestClass]
-    public class UnitTest1
+    public class Password
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForSmallLetters()
         {
+            var password= GenerateLettersAndNumbers(3, 'a', 'z' + 1);
+            Assert.AreEqual(3, CountSmallLetters(password));
+        }
+
+        string GenerateLettersAndNumbers(int number, int start, int end)
+        {
+            string result = null;
+            for (int i = 0; i < number; i++)
+            {
+                Random random = new Random();
+                char character = (char)random.Next(start,end);
+                result += character.ToString();
+            }
+            return result;
+        }
+
+        int CountSmallLetters(string password)
+        {
+            int count = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] >= 'a' && password[i] <= 'z')
+                    count++;
+            }
+            return count;
         }
     }
 }
