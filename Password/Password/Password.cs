f1e6de66abfaef197c1b81467ca33c2dfa904f03
ElenaCiocan <elenaciocan94@gmail.com>
2016-03-27 18:47:23 +0300

    Modified version to eliminate similar and ambiguous characters

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -9,7 +9,7 @@ namespace Password
         [TestMethod]
         public void TestForSmallLetters()
         {
-            var password= GeneratePassword(new Option[] { new Option(1, 4), new Option(2, 2)});
+            var password= GeneratePassword(new Option[] { new Option(1, 4,), new Option(2, 2)});
             Assert.AreEqual(4, CountCharacters(password,'a','z'));
         }
 
@@ -35,15 +35,25 @@ namespace Password
             Assert.AreEqual(5, CountSymbols(password,symbols));
         }
 
+        [TestMethod]
+        public void TestforEliminateCharacters()
+        {
+            Assert.AreEqual
+        }
+
         struct Option
         {
             public int optionNumber;
             public int length;
+            public bool eliminateSimilarCharacters;
+            public bool eliminateAmbiguousCharacters;
 
-            public Option(int optionNumber, int length)
+            public Option(int optionNumber, int length, bool eliminateSimilarCharacters=false, bool eliminateAmbiguousCharacters=false)
             {
                 this.optionNumber = optionNumber;
                 this.length = length;
+                this.eliminateSimilarCharacters = eliminateSimilarCharacters;
+                this.eliminateAmbiguousCharacters = eliminateAmbiguousCharacters;
             }
         }
         string GeneratePassword(Option[] options)
@@ -53,38 +63,72 @@ namespace Password
             for (int i = 0; i < options.Length; i++)
             {
                 if (options[i].optionNumber == 1)
-                    password += GenerateLettersAndNumbers(options[i].length, 'a', 'z' + 1);
+                    password += GenerateLettersAndNumbers(options[i].length, 'a', 'z' + 1,options[i].eliminateSimilarCharacters);
                 if (options[i].optionNumber == 2)
-                    password += GenerateLettersAndNumbers(options[i].length, 'A', 'Z' + 1);
+                    password += GenerateLettersAndNumbers(options[i].length, 'A', 'Z' + 1,options[i].eliminateSimilarCharacters);
                 if (options[i].optionNumber == 3)
-                    password += GenerateLettersAndNumbers(options[i].length, 0, 10);
+                    password += GenerateLettersAndNumbers(options[i].length, 0, 10, options[i].eliminateSimilarCharacters);
                 if (options[i].optionNumber == 4)
-                    password += GenerateSymbols(options[i].length, symbols);
-
+                    password += GenerateSymbols(options[i].length, symbols,options[i].eliminateAmbiguousCharacters);
             }
             return password;
         }
 
-        string GenerateLettersAndNumbers(int number, int start, int end)
+        string GenerateLettersAndNumbers(int number, int start, int end, bool eliminateCharacters, string charactersToEliminate = "l1Io0O")
         {
+            Random random = new Random();
             string result = null;
-            for (int i = 0; i < number; i++)
+            char[] eliminate = charactersToEliminate.ToCharArray(0, charactersToEliminate.Length);
+            if(eliminateCharacters)
             {
-                Random random = new Random();
-                char character = (char)random.Next(start,end);
-                result += character.ToString();
+                result = NewMethod(number, start, end, random, result, eliminate);
+            }
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    char character = (char)random.Next(start, end);
+                    result += character.ToString();
+                }
             }
             return result;
         }
 
-        string GenerateSymbols(int number, string symbols)
+        private static string NewMethod(int number, int start, int end, Random random, string result, char[] eliminate)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                char character = (char)random.Next(start, end);
+                while (Array.IndexOf(eliminate, character) >= 0)
+                    character = (char)random.Next(start, end);
+                result += character.ToString();
+            }
+
+            return result;
+        }
+
+        string GenerateSymbols(int number, string symbols, bool eliminateCharacters, string charactersToEliminate = "{}[]()/\'~,;.<>")
         {
             string result = null;
             Random random = new Random();
-            for (int i = 0; i < number; i++)
+            char[] eliminate = charactersToEliminate.ToCharArray(0, charactersToEliminate.Length);
+            if (eliminateCharacters)
             {
-                int position = random.Next(0,symbols.Length);
-                result += symbols[position];
+                for (int i = 0; i < number; i++)
+                {
+                    int position = random.Next(0, symbols.Length);
+                    while (Array.IndexOf(eliminate, symbols[position]) >= 0)
+                        position = random.Next(0, symbols.Length);
+                    result += symbols[position];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    int position = random.Next(0, symbols.Length);
+                    result += symbols[position];
+                }
             }
             return result;
         }
