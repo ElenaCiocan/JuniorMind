be8895c4bb9048d5676310ed8db53a18f73e4dd1
ElenaCiocan <elenaciocan94@gmail.com>
2016-04-05 23:16:55 +0300

    Test for reversing a word

--- Reverse/Reverse/Reverse.cs
+++ Reverse/Reverse/Reverse.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Reverse
 {
     [TestClass]
-    public class UnitTest1
+    public class Reverse
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForReversingAWord()
         {
+            Assert.AreEqual("cba", ReverseAWord("abc"));
+        }
+
+        string ReverseAWord(string word)
+        {
+            if (word.Length == 1)
+                return word;
+            return word[word.Length - 1] + ReverseAWord(word.Substring(0, word.Length - 1));
         }
     }
 }
