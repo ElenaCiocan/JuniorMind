57fc32854d6970e49b34dde2228e1e85f9d011e5
ElenaCiocan <elenaciocan94@gmail.com>
2016-04-06 00:46:10 +0300

    Test for checking the replacement

--- ProblemReplace/ProblemReplace/ProblemReplace.cs
+++ ProblemReplace/ProblemReplace/ProblemReplace.cs
@@ -4,11 +4,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ProblemReplace
 {
     [TestClass]
-    public class UnitTest1
+    public class ProblemReplace
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForReplacingACharacter()
         {
+            Assert.AreEqual("okaokcdokc", ReplaceACharacter("babcdbc", 'b', "ok"));
+        }
+
+        string ReplaceACharacter(string initialString, char characterToReplace, string stringForReplacing)
+        {
+            if (initialString.Length == 1)
+            {
+                if (initialString[0] == characterToReplace)
+                    return stringForReplacing;
+                else
+                    return initialString[0].ToString();
+            }
+            else
+            if (initialString[0] == characterToReplace)
+                return stringForReplacing + ReplaceACharacter(initialString.Substring(1, initialString.Length - 1), characterToReplace, stringForReplacing);
+
+            return initialString[0] + ReplaceACharacter(initialString.Substring(1, initialString.Length - 1), characterToReplace, stringForReplacing);
+        }
+              
         }
     }
-}
+
