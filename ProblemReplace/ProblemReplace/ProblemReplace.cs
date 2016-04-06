b52fa3672552472af4d72c1953b6947143765bd3
ElenaCiocan <elenaciocan94@gmail.com>
2016-04-06 16:25:21 +0300

    Refactorization for Reverse

--- ProblemReplace/ProblemReplace/ProblemReplace.cs
+++ ProblemReplace/ProblemReplace/ProblemReplace.cs
@@ -14,20 +14,13 @@ namespace ProblemReplace
 
         string ReplaceACharacter(string initialString, char characterToReplace, string stringForReplacing)
         {
-            if (initialString.Length == 1)
-            {
-                if (initialString[0] == characterToReplace)
-                    return stringForReplacing;
-                else
-                    return initialString[0].ToString();
-            }
-            else
+            if (String.IsNullOrEmpty(initialString))
+                return string.Empty;
             if (initialString[0] == characterToReplace)
                 return stringForReplacing + ReplaceACharacter(initialString.Substring(1, initialString.Length - 1), characterToReplace, stringForReplacing);
-
             return initialString[0] + ReplaceACharacter(initialString.Substring(1, initialString.Length - 1), characterToReplace, stringForReplacing);
         }
-              
-        }
+
     }
+}
 
