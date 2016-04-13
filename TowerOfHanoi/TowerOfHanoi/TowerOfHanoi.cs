03c7fc9118540159d233eaae23955cd0465f283d
ElenaCiocan <elenaciocan94@gmail.com>
2016-04-13 12:14:36 +0300

    Test for 3 disks

--- TowerOfHanoi/TowerOfHanoi/TowerOfHanoi.cs
+++ TowerOfHanoi/TowerOfHanoi/TowerOfHanoi.cs
@@ -4,11 +4,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TowerOfHanoi
 {
     [TestClass]
-    public class UnitTest1
+    public class TowerOfHanoi
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForMovingThreeDisks()
         {
+            int[] source = { 3, 2, 1 };
+            int[] destination = new int[source.Length];
+            int[] auxiliar = new int[source.Length];
+
+            CollectionAssert.AreEqual(new int[] { 3, 2, 1 }, MovingDisksUsingTowerOfHanoi(3, source, destination, auxiliar));
+        }
+
+        int[] MovingDisksUsingTowerOfHanoi(int noOfDisks, int[] sourcePeg, int[] destinationPeg, int[] auxiliarPeg)
+        {
+            if (noOfDisks == 1)
+            {
+                return MovingDisks(noOfDisks, sourcePeg, destinationPeg);
+            }
+
+            MovingDisksUsingTowerOfHanoi(noOfDisks - 1, sourcePeg, auxiliarPeg, destinationPeg);
+            MovingDisks(noOfDisks, sourcePeg, destinationPeg);
+            MovingDisksUsingTowerOfHanoi(noOfDisks - 1, auxiliarPeg, destinationPeg, sourcePeg);
+            return destinationPeg;
+        }
+
+        private static int[] MovingDisks(int noOfDisks, int[] source, int[] destination)
+        {
+            destination[noOfDisks - 1] = source[noOfDisks - 1];
+            Array.Resize(ref source, source.Length - 1);
+            return source;
         }
     }
 }
