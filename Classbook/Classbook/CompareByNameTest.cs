e2305859f9641ee492c667e6ee6bb3d3577fc869
ElenaCiocan <elenaciocan94@gmail.com>
2016-05-09 01:02:40 +0300

    Test for comparison by name

---
+++ Classbook/Classbook/CompareByNameTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Classbook
+{
+    [TestClass]
+    public class CompareByNameTest
+    {
+        [TestMethod]
+        public void TestForCamparisonByName()
+        {
+            var iulian = new Pupil("Iulian Popescu");
+            var maria = new Pupil("Maria Apetrii");
+
+            Assert.AreEqual(-1, iulian.CompareByName(maria));
+        }
+    }
+}
---
+++ Classbook/Classbook/Pupil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classbook
+{
+    class Pupil
+    {
+        private string name;
+        public Pupil( string name)
+        {
+            this.name = name;
+        }
+
+        public int CompareByName(Pupil pupil)
+        {
+            return String.Compare(name, pupil.name);
+        }
+    }
+}
