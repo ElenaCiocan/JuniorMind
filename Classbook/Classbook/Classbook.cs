e0da20424a5a1706b5fdb77a0a80c9c8796718f2
ElenaCiocan <elenaciocan94@gmail.com>
2016-05-09 00:08:06 +0300

    Problem  Classbook blank project

---
+++ Classbook/Classbook/Classbook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Classbook
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
