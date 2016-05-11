364f4a699da3f580d5d9cd27db98b2a3edd69ead
ElenaCiocan <elenaciocan94@gmail.com>
2016-05-11 23:27:52 +0300

    Test for sorted pupils

---
+++ Classbook/Classbook/SortedPupils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classbook
+{
+    class SortedPupils
+    {
+        private Pupil[] pupils;
+
+        public SortedPupils(Pupil[] pupils)
+        {
+            this.pupils = pupils;
+        }
+
+        public Pupil[] SortAlphabetically(Pupil[] pupils, int start, int end)
+        {
+            int i = start;
+            int j = end;
+            Pupil pivot = pupils[(start + end) / 2], auxiliar;
+
+            while (i <= j)
+            {
+                while (pupils[i].CompareByName(pivot) == -1)
+                    i++;
+
+                while (pupils[j].CompareByName(pivot) == 1)
+                    j--;
+
+                if (i <= j)
+                {
+                    auxiliar = pupils[i];
+                    pupils[i] = pupils[j];
+                    pupils[j] = auxiliar;
+                    i++;
+                    j--;
+                }
+                if (start < j)
+                    SortAlphabetically(pupils, start, j);
+                if (i < end)
+                    SortAlphabetically(pupils, i, end);
+            }
+
+            return pupils;
+
+        }
+    }
+}
---
+++ Classbook/Classbook/SortedPupilsTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classbook
+{
+    [TestClass]
+    public class SortedPupilsTest
+    {
+        [TestMethod]
+        public void TestForSortedPupils()
+        {
+            var iulian = new Pupil("Iulian Popescu");
+            var maria = new Pupil("Maria Apetrii");
+            var adrian = new Pupil("Adrian Ghisoui");
+            var nico = new Pupil("Nicoleta Raiu");
+            var elena = new Pupil("Elena Ciocan");
+            var pupils = new Pupil[]{ iulian, maria, adrian, nico, elena };
+            var sortedPupils = new Pupil[] { adrian, elena, iulian, maria, nico };
+            var list = new SortedPupils(pupils);
+
+            CollectionAssert.AreEqual(sortedPupils, list.SortAlphabetically(pupils,0,pupils.Length-1));
+
+        }
+
+    }
+}
