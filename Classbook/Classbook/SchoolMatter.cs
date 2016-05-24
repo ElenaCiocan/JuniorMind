629f1e75b80b12189f066f28be16563ce8ae6e90
ElenaCiocan <elenaciocan94@gmail.com>
2016-05-24 21:15:13 +0300

    Test for arithmetical mean

---
+++ Classbook/Classbook/SchoolMatter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classbook
+{
+    class SchoolMatter
+    {
+        private int[] grades;
+
+        public SchoolMatter(int[] grades)
+        {
+            this.grades = grades;
+        }
+
+        public double CalculateArithmeticalMean( int[] grades)
+        {
+            double mean = 0, arithmeticalMean=0;
+            for (int i = 0; i < grades.Length; i++)
+                mean += grades[i];
+            arithmeticalMean = mean / grades.Length;
+            return arithmeticalMean;
+        }
+    }
+}
---
+++ Classbook/Classbook/SchoolMatterTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classbook
+{
+    [TestClass]
+    public class SchoolMatterTest
+    {
+        [TestMethod]
+        public void TestForArithmeticalMean()
+        {
+            var grades = new int[]{9,8,6};
+            var mathematics = new SchoolMatter(grades);
+            Assert.AreEqual(7.66, mathematics.CalculateArithmeticalMean(grades), 1e-2);
+        }
+    }
+}
