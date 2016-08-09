4e95b45304e1da3d8f756307726d212363be0efa
ElenaCiocan <elenaciocan94@gmail.com>
2016-08-09 21:04:35 +0300

    Test for Add and Count

--- List/List/List.cs
+++ List/List/List.cs
@@ -1,13 +1,97 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using 
 
 namespace List
 {
-    class List
+    class List<T> : IList<T>
     {
+        T[] numbers = new T[2];
+        int count = 0;
+        
+        public T this[int index]
+        {
+            get
+            {
+                return numbers[index];
+            }
+
+            set
+            {
+                numbers[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref numbers, numbers.Length + 1);
+            numbers[count] = item;
+            count++;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var no in numbers)
+                yield return no;
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
--- List/List/ListTest.cs
+++ List/List/ListTest.cs
@@ -1,14 +1,27 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace List
 {
-    [TestClass]
     public class ListTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void TestForAdd()
         {
+            var listOfNumbers = new List<int>();
+            listOfNumbers.Add(25);
+
+            Assert.Equal(new[]{ 25,0,0}, listOfNumbers);
+        }
+        
+        [Fact]
+        public void TestForCount()
+        {
+            var listOfNumbers = new List<int>();
+            listOfNumbers.Add(25);
+            listOfNumbers.Add(8);
+
+            Assert.Equal(2,listOfNumbers.Count);
         }
     }
 }
