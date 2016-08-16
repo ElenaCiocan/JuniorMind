a95e9ad5afc8700c5a846862d8c2bdb4eee46c07
ElenaCiocan <elenaciocan94@gmail.com>
2016-08-17 01:04:54 +0300

    Create and first test for linked list

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList<T> : IEnumerable<T>
+    {
+        private Node<T> sentinel;
+        private int noOfNodes;
+
+        public LinkedList()
+        {
+            sentinel = new Node<T>(default(T));
+            sentinel.Next = sentinel;
+            sentinel.Previous = sentinel;
+            noOfNodes = 0;
+        }
+
+        public void Add(T value)
+        {
+            AddAtTheEnd(value);
+        }
+
+        public void AddAtTheEnd(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            node.Next = sentinel;
+            node.Previous = sentinel.Previous;
+            sentinel.Previous.Next = node;
+            sentinel.Previous = node;
+
+            noOfNodes++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> node = sentinel.Next;
+            while (!node.Equals(sentinel))
+            {
+                yield return node.Data;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+}
---
+++ LinkedList/LinkedList/LinkedListTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace LinkedList
+{
+    public class LinkedListTest
+    {
+        [Fact]
+        public void TestForAddLast()
+        {
+           var list = new LinkedList<int> { 1, 2 };
+            list.AddAtTheEnd(3);
+            Assert.Equal(new[] { 1, 2, 3 }, list);
+
+        }
+    }
+}
